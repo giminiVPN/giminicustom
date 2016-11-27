8bcd9b2e5a1b0d6648ba3e62ab434e103a54d873
iKewell <kewell.tsao@qq.com>
2016-11-27 14:27:11 +0800

    添加项目

---
+++ GeminiVPN/App.xaml.cs
@@ -0,0 +1,95 @@
+﻿using GeminiVPN.ViewModels;
+using GeminiVPN.Views;
+using log4net;
+using log4net.Config;
+using Microsoft.Practices.Unity;
+using Prism.Logging;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GeminiVPN
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            new Bootstrapper(this).Run();
+        }
+
+        public class Bootstrapper : UnityBootstrapper
+        {
+            public Bootstrapper(App app)
+            {
+                Application = app;
+            }
+
+            public App Application { get; }
+
+            protected override ILoggerFacade CreateLogger()
+            {
+                XmlConfigurator.Configure();
+
+                return new BootstrapperLogger();
+            }
+
+            protected override void ConfigureContainer()
+            {
+                base.ConfigureContainer();
+
+                Container.RegisterType<MainShell>();
+                Container.RegisterType<MainShellViewModel>();
+            }
+
+            protected override DependencyObject CreateShell()
+            {
+                return Container.Resolve<MainShell>();
+            }
+
+            protected override void InitializeShell()
+            {
+                Application.MainWindow = (Window)Shell;
+                Application.MainWindow.Show();
+            }
+        }
+
+        public class BootstrapperLogger : ILoggerFacade
+        {
+            public ILog Logger { get; }
+
+            public BootstrapperLogger()
+            {
+                Logger = LogManager.GetLogger(typeof(Bootstrapper));
+            }
+
+            public void Log(string message, Category category, Priority priority)
+            {
+                switch (category)
+                {
+                    case Category.Debug:
+                        Logger.DebugFormat("[{0}] {1}", priority, message);
+                        break;
+                    case Category.Exception:
+                        Logger.ErrorFormat("[{0}] {1}", priority, message);
+                        break;
+                    case Category.Info:
+                        Logger.InfoFormat("[{0}] {1}", priority, message);
+                        break;
+                    case Category.Warn:
+                        Logger.WarnFormat("[{0}] {1}", priority, message);
+                        break;
+                    default:
+                        Logger.InfoFormat("[{0}] {1}", priority, message);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ GeminiVPN/Views/MainShell.xaml.cs
@@ -0,0 +1,34 @@
+﻿using GeminiVPN.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Practices.Unity;
+
+namespace GeminiVPN.Views
+{
+    public partial class MainShell : Window
+    {
+        public MainShell()
+        {
+            InitializeComponent();
+        }
+
+        [Dependency]
+        public new MainShellViewModel DataContext
+        {
+            get { return base.DataContext as MainShellViewModel; }
+            set { base.DataContext = value; }
+        }
+    }
+}
